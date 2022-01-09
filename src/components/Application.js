3bb53bb3ff1a2cae48f31b8c7e0e8c338e757d6a
brogr <dev@brogramming.com>
2022-01-09 21:05:14 +0100

    include Appointment(s) in Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,31 +1,92 @@
 import React, { useState } from "react";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 import "components/Application.scss";
 
-export default function Application(props) {
-  // Test data for DayList
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
+// Test data for DayList
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+// Test data for Appointment
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
     },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Hans Heiri",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      },
     },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Ann Mitchell",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      },
     },
-  ];
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Sirah Cypress",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+];
 
+export default function Application(props) {
   // state
   const [day, setDay] = useState("Monday");
   // console.log(day);
+
+  const AppointmentList = appointments.map((appointment) => (
+    <Appointment key={appointment.id} {...appointment} />
+  ));
+  AppointmentList.push(<Appointment key="last" time="5pm" />);
 
   return (
     <main className="layout">
@@ -45,9 +106,7 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
+      <section className="schedule">{AppointmentList}</section>
     </main>
   );
 }
