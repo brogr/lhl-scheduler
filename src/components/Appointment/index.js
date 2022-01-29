8d7e97a87d301690fdf8c9da31f5a19b3d63d664
brogr <dev@brogramming.com>
2022-01-29 21:30:35 +0100

    updating spots; some refactoring

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -22,8 +22,6 @@ const ERROR_SAVE = "ERROR_SAVE";
 const ERROR_DELETE = "ERROR_DELETE";
 
 export default function Appointment(props) {
-	console.log(props);
-	
   // history handling
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function useApplicationData() {
+  // url for appointments api
+  const API_APPOINTMENTS_URL = "/api/appointments/";
+
 	// combined state
 	const [state, setState] = useState({
 		day: "Monday",
@@ -13,6 +16,36 @@ function useApplicationData() {
 	// Aliased actions for combined state
 	const setDay = (day) => setState({ ...state, day });
 	// const setDays = (days) => setState((prev) => ({ ...prev, days }));
+
+  // receives updated/new appointments to calculate spots remaining (instead of relying on outdated state)
+  function updateSpots(appointments) {
+		// 0. create new days array to update spots
+		// const newDays = [...state.days]; // problem: shallow copy only because multi-dimensional array
+		const newDays = Array.from(state.days); // deep clone
+
+		// 1. get the day object
+		const dayObj = newDays.find((day) => day.name === state.day);
+		// 2. count the day's appointments without interviews to determine spots available
+		const spots = dayObj.appointments.filter(
+			(appId) => appointments[appId].interview === null
+		).length;
+		// console.log(spots);
+		// 3. update spots
+		dayObj.spots = spots;
+		// const newDay = {...dayObj, spots}
+
+		return newDays;
+  }
+  
+  // handle state updates after API calls (update/delete)
+  function processAPIResponse(response, appointments) {
+		if (response.status === 204) {
+			// update spots and get new days
+			const days = updateSpots(appointments);
+			// update state: setState with the new state object
+			setState({ ...state, appointments, days });
+		}
+	}
 
 	// update the local state when we book an interview
 	function bookInterview(id, interview) {
@@ -30,13 +63,9 @@ function useApplicationData() {
 
 		// update DB & state
 		return axios
-			.put("/api/appointments/" + id, { interview })
+			.put(API_APPOINTMENTS_URL + id, { interview })
 			.then((response) => {
-				if (response.status === 204) {
-					// console.log("saved", response);
-					// update state: setState with the new state object
-					setState({ ...state, appointments });
-				}
+				processAPIResponse(response, appointments);
 			});
 	}
 
@@ -54,12 +83,8 @@ function useApplicationData() {
 		};
 
 		// update DB & state
-		return axios.delete("/api/appointments/" + id).then((response) => {
-			if (response.status === 204) {
-				// console.log("saved", response);
-				// update state: setState with the new state object
-				setState({ ...state, appointments });
-			}
+		return axios.delete(API_APPOINTMENTS_URL + id).then((response) => {
+			processAPIResponse(response, appointments);
 		});
 	}
 
@@ -80,9 +105,9 @@ function useApplicationData() {
 				interviewers: interviewers.data,
 			}));
 		});
-  }, []);
-  
-  return { state, setDay, bookInterview, cancelInterview };
+	}, []);
+
+	return { state, setDay, bookInterview, cancelInterview };
 }
 
 export default useApplicationData;
