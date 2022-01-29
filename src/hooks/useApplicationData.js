6f190a870b4813b81a890c661313b2f80e6d0e49
brogr <dev@brogramming.com>
2022-01-29 22:42:48 +0100

    refactored: replaced useState with useReducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,12 +1,72 @@
-import React, { useState, useEffect } from "react";
+import { React, useEffect, useReducer } from "react";
 import axios from "axios";
+
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
+function reducer(state, action) {
+	switch (action.type) {
+		case SET_DAY:
+			return {
+        ...state,
+        day: action.day
+			};
+		case SET_APPLICATION_DATA:
+			return {
+				...state,
+				days: action.days,
+				appointments: action.appointments,
+				interviewers: action.interviewers
+			};
+    case SET_INTERVIEW: {
+			// create a new appointment object by copying existing appointment to make sure that the object is not shared
+			const appointment = {
+				...state.appointments[action.id],
+				interview: action.interview,
+			};
+			// update the appointments object with immutable pattern
+			const appointments = {
+				...state.appointments,
+				[action.id]: appointment,
+			};
+			// update spots and get new days
+			const days = updateSpots(state, appointments);
+
+			return { ...state, appointments, days };
+		}
+		default:
+			throw new Error(
+				`Tried to reduce with unsupported action type: ${action.type}`
+			);
+	}
+}
+
+// receives updated/new appointments to calculate spots remaining (instead of relying on outdated state)
+function updateSpots(state, appointments) {
+  // 0. create new days array to update spots
+  // const newDays = [...state.days]; // problem: shallow copy only because multi-dimensional array
+  const newDays = Array.from(state.days); // deep clone
+  // 1. get the day object
+  const dayObj = newDays.find((day) => day.name === state.day);
+  // 2. count the day's appointments without interviews to determine spots available
+  const spots = dayObj.appointments.filter(
+    (appId) => appointments[appId].interview === null
+  ).length;
+  // console.log(spots);
+  // 3. update spots
+  dayObj.spots = spots;
+  // const newDay = {...dayObj, spots}
+  return newDays;
+}
+
 
 function useApplicationData() {
   // url for appointments api
   const API_APPOINTMENTS_URL = "/api/appointments/";
 
-	// combined state
-	const [state, setState] = useState({
+  // reducer (instead of complex useState)
+  const [state, dispatch] = useReducer(reducer, {
 		day: "Monday",
 		days: [],
 		appointments: {},
@@ -14,77 +74,29 @@ function useApplicationData() {
 	});
 
 	// Aliased actions for combined state
-	const setDay = (day) => setState({ ...state, day });
-	// const setDays = (days) => setState((prev) => ({ ...prev, days }));
-
-  // receives updated/new appointments to calculate spots remaining (instead of relying on outdated state)
-  function updateSpots(appointments) {
-		// 0. create new days array to update spots
-		// const newDays = [...state.days]; // problem: shallow copy only because multi-dimensional array
-		const newDays = Array.from(state.days); // deep clone
-
-		// 1. get the day object
-		const dayObj = newDays.find((day) => day.name === state.day);
-		// 2. count the day's appointments without interviews to determine spots available
-		const spots = dayObj.appointments.filter(
-			(appId) => appointments[appId].interview === null
-		).length;
-		// console.log(spots);
-		// 3. update spots
-		dayObj.spots = spots;
-		// const newDay = {...dayObj, spots}
-
-		return newDays;
-  }
-  
-  // handle state updates after API calls (update/delete)
-  function processAPIResponse(response, appointments) {
-		if (response.status === 204) {
-			// update spots and get new days
-			const days = updateSpots(appointments);
-			// update state: setState with the new state object
-			setState({ ...state, appointments, days });
-		}
-	}
+  const setDay = (day) => dispatch({ type: SET_DAY, day });
 
 	// update the local state when we book an interview
 	function bookInterview(id, interview) {
-		// console.log(id, interview);
-		// create a new appointment object by copying existing appointment to make sure that the object is not shared
-		const appointment = {
-			...state.appointments[id],
-			interview: { ...interview },
-		};
-		// update the appointments object with immutable pattern
-		const appointments = {
-			...state.appointments,
-			[id]: appointment,
-		};
-
 		// update DB & state
 		return axios
 			.put(API_APPOINTMENTS_URL + id, { interview })
 			.then((response) => {
-				processAPIResponse(response, appointments);
+				if (response.status === 204) {
+					// dispatch to update reducer state
+					dispatch({ type: SET_INTERVIEW, id, interview });
+				}
 			});
 	}
 
 	// delete interview
 	function cancelInterview(id) {
-		// create a new appointment object by copying existing appointment to make sure that the object is not shared
-		const appointment = {
-			...state.appointments[id],
-			interview: null,
-		};
-		// update the appointments object with immutable pattern
-		const appointments = {
-			...state.appointments,
-			[id]: appointment,
-		};
-
 		// update DB & state
 		return axios.delete(API_APPOINTMENTS_URL + id).then((response) => {
-			processAPIResponse(response, appointments);
+			if (response.status === 204) {
+				// dispatch to update reducer state
+				dispatch({ type: SET_INTERVIEW, id, interview: null });
+			}
 		});
 	}
 
@@ -97,13 +109,13 @@ function useApplicationData() {
 		]).then((all) => {
 			// console.log(all);
 			const [days, appointments, interviewers] = all;
-			// set combined state at once
-			setState((prev) => ({
-				...prev,
+			// dispatch to update reducer state
+			dispatch({
+				type: SET_APPLICATION_DATA,
 				days: days.data,
 				appointments: appointments.data,
 				interviewers: interviewers.data,
-			}));
+			});
 		});
 	}, []);
 
