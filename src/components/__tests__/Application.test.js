78194ba196270eecf6d86552eaf6fb48c51556b6
brogr <dev@brogramming.com>
2022-01-30 17:39:59 +0100

    Added tests for Application: load data and book interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,32 +1,61 @@
 import React from "react";
-
 import {
 	render,
 	cleanup,
 	waitForElement,
-	fireEvent,
+  fireEvent,
+  getByText,
+  getAllByTestId,
+  getByAltText,
+  getByPlaceholderText,
+	prettyDOM,
 } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-xit("defaults to Monday and changes the schedule when a new day is selected (Promise)", () => {
-	const { getByText } = render(<Application />);
+describe("Application", () => {
+	xit("defaults to Monday and changes the schedule when a new day is selected (Promise)", () => {
+		const { getByText } = render(<Application />);
 
-	return waitForElement(() => getByText("Monday")).then(() => {
+		return waitForElement(() => getByText("Monday")).then(() => {
+			fireEvent.click(getByText("Tuesday"));
+			expect(getByText("Leopold Silvers")).toBeInTheDocument();
+		});
+	});
+
+	it("defaults to Monday and changes the schedule when a new day is selected (Async Await)", async () => {
+		const { getByText } = render(<Application />);
+
+		await waitForElement(() => getByText("Monday"));
+
 		fireEvent.click(getByText("Tuesday"));
+
 		expect(getByText("Leopold Silvers")).toBeInTheDocument();
 	});
 
-});
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    // const appointments = getAllByTestId(container, "appointment");
+    // console.log(prettyDOM(appointments));
+    const appointment = getAllByTestId(container, "appointment")[0];
+		// console.log(prettyDOM(appointment));
 
-it("defaults to Monday and changes the schedule when a new day is selected (Async Await)", async () => {
-	const { getByText } = render(<Application />);
+    fireEvent.click(getByAltText(appointment, "Add"));
 
-	await waitForElement(() => getByText("Monday"));
+		fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+			target: { value: "Lydia Miller-Jones" },
+		});
+		fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
-	fireEvent.click(getByText("Tuesday"));
+		fireEvent.click(getByText(appointment, "Save"));
 
-	expect(getByText("Leopold Silvers")).toBeInTheDocument();
+
+
+	});
+
+
+
 });
